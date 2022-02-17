f8f363d5bca0aee0d5c92ceac47622ab7b3c114f
Vidhyasagar005 <kumarvidhya005@gmail.com>
2022-02-18 02:11:25 +0530

    Add project files.

---
+++ calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res;
+            Console.Write("Enter the  first number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the symbole(/,*,+,-)");
+            string symbole = Console.ReadLine();
+            switch(symbole)
+            {
+                case "+":
+                    res = num + num2;
+                    Console.WriteLine("Addition" + res);
+                    break;
+                case "-":
+                    res = num - num2;
+                    Console.WriteLine("Subtraction" + res);
+                    break;
+                case "*":
+                    res = num * num2;
+                    Console.WriteLine("Multiplication" + res);
+                    break;
+                case "/":
+                    res = num / num2;
+                    Console.WriteLine("Division" + res);
+                    break;
+                default:
+                    Console.WriteLine("Wrong input");
+                    break;
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
